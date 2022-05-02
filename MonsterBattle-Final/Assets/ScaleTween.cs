c2d1a5ba1a3b6823f8ae4561ee9f97fb6cba11bb
SCastilloEdu <102997370+SCastilloEdu@users.noreply.github.com>
2022-05-02 17:31:36 -0400

    Fixed bullet, added GM
    - Fixed bullet to no longer fly off into corner, properly targets the selected monster
    - Began making the game manager

--- MonsterBattle-Final/Assets/ScaleTween.cs
+++ MonsterBattle-Final/Assets/ScaleTween.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class ScaleTween : MonoBehaviour
 {
-    public GameObject Attacker;
     public GameObject Defender;
+    public float delay = 0f;
+    private float defPos = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        transform.LeanMoveLocal(Defender.transform.position,20).setEaseOutCubic();
+        defPos = Defender.GetComponent<RectTransform>().anchoredPosition.x;
+
+        LeanTween.moveX(gameObject.GetComponent<RectTransform>(), defPos, 0.3f).setDelay(delay).setOnComplete(Explode);
+    }
+
+    void Explode()
+    {
+        LeanTween.scale(gameObject, new Vector3(10,10,10), 0.2f).setOnComplete(DestroyMe);
+        LeanTween.rotate(gameObject, new Vector3(0, 0, 180), 0.2f).setOnComplete(DestroyMe);
+        LeanTween.alpha(gameObject.GetComponent<RectTransform>(), 0f, 0.2f).setOnComplete(DestroyMe);
+    }
+
+    void DestroyMe()
+    {
+        Destroy(gameObject);
     }
 }
