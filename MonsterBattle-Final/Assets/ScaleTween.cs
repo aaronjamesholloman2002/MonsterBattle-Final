ad29ed3278bd6243c10e3ce62d9ce0e03f101f78
SCastilloEdu <102997370+SCastilloEdu@users.noreply.github.com>
2022-05-02 19:47:39 -0400

    Button Menu Functions
    - Made the attack and item buttons function, attacks are unfinished and spawn under the player window
    - Tweaked the HP bar to no longer overflow and use whole numbers for when units have actual HP values
    - Began work on object pooling manager for when attacks are spawned

--- MonsterBattle-Final/Assets/ScaleTween.cs
+++ MonsterBattle-Final/Assets/ScaleTween.cs
@@ -7,19 +7,33 @@ public class ScaleTween : MonoBehaviour
     public GameObject Defender;
     public float delay = 0f;
     private float defPos = 0f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        defPos = Defender.GetComponent<RectTransform>().anchoredPosition.x;
+    public bool isPlayerAttacker = true;
 
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        /*defPos = Defender.GetComponent<RectTransform>().anchoredPosition.x;
+        if (isPlayerAttacker == true)
+        {
+            gameObject.GetComponent<ScaleTween>().Defender = enemy;
+        }
+        else
+        {
+            gameObject.GetComponent<ScaleTween>().Defender = player;
+        }*/
+
+        Move();
+    }
+
+    void Move()
+    {
         LeanTween.moveX(gameObject.GetComponent<RectTransform>(), defPos, 0.3f).setDelay(delay).setOnComplete(Explode);
     }
 
     void Explode()
     {
-        LeanTween.scale(gameObject, new Vector3(10,10,10), 0.2f).setOnComplete(DestroyMe);
-        LeanTween.rotate(gameObject, new Vector3(0, 0, 180), 0.2f).setOnComplete(DestroyMe);
+        LeanTween.scale(gameObject, new Vector3(10,10,10), 0.2f);
+        LeanTween.rotate(gameObject, new Vector3(0, 0, 180), 0.2f);
         LeanTween.alpha(gameObject.GetComponent<RectTransform>(), 0f, 0.2f).setOnComplete(DestroyMe);
     }
 
---
+++ MonsterBattle-Final/Assets/Scripts/Game Scripts/ButtonMenuScript.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenuScript : MonoBehaviour
+{
+    public GameObject main;
+    public GameObject atk;
+    public GameObject item;
+
+    public GameObject Bullet;
+    // Main Buttons
+    public void AttackButton()
+    {
+        atk.SetActive(true);
+    }
+
+    public void ItemButton()
+    {
+        item.SetActive(true);
+    }
+
+    public void InfoButton()
+    {
+
+    }
+
+    public void TeamButton()
+    {
+
+    }
+
+    public void ForfeitButton()
+    {
+
+    }
+
+    // Attack Buttons
+    public void AtkReturn()
+    {
+        atk.SetActive(false);
+    }
+
+    public void Atk1()
+    {
+        GameObject bulletInstance = Instantiate(Bullet);
+        bulletInstance.transform.SetParent(GameObject.Find("ObjectPoolingManager").transform,true);
+    }
+
+    public void Atk2()
+    {
+
+    }
+
+    public void Atk3()
+    {
+
+    }
+
+    public void Atk4()
+    {
+
+    }
+
+    // Item Buttons
+    public void ItemReturn()
+    {
+        item.SetActive(false);
+    }
+
+    public void Item1()
+    {
+
+    }
+
+    public void Item2()
+    {
+
+    }
+
+    public void Item3()
+    {
+
+    }
+
+    public void Item4()
+    {
+
+    }
+}
